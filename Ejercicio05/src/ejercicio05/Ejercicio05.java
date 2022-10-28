0dcca48d86b7d4ab28dcd327f2e392d776348389
Steven Cuenca <626tellor@gmail.com>
2022-10-28 13:05:09 -0500

    Taller03

--- Ejercicio05/src/ejercicio05/Ejercicio05.java
+++ Ejercicio05/src/ejercicio05/Ejercicio05.java
@@ -3,33 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejercicio04;
+package ejercicio05;
 
 /**
  *
  * @author UTPL
  */
-public class Ejercicio04 {
+public class Ejercicio05 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int var1 = 20;
-        int var2 = 1;
-        int var3 = 3;
-        int var4 = 2;
+        
+        int var1 = 2;
+        int var2 = 2;
+        int var3 = 12;
+        int var4 = 1;
         int var5 = 3;
         
         System.out.println("-----------Solución a problema-----------");
         
-        double valor = var1 / (var2 + var3) + var4;
+        double valor = (Math.pow(var1, var2) + var3 -var4) / 3;
         
-        boolean var = valor >= var5;
-        
-        System.out.println(var);
-        
+        System.out.println(valor);
+
     }
     
 }
